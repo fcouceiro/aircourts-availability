1e7ab96f7dc9d55dad27112bd55c57edbcc42d73
Francisco Couceiro <couceiro.f@gmail.com>
2023-10-24 00:43:02 +0100

    Prepare ElCourts for public release

--- express.js
+++ express.js
@@ -1,14 +1,10 @@
-const airCourtsWrapper = require('./index')
 const moment = require('moment')
 const express = require('express')
-const ddb = require('./aws/ddb')
-const lambda = require('./aws/lambda')
 const { any, isNil, either, isEmpty, flatten } = require('ramda')
 const Subscriptions = require('./subscriptions')
 const airCourtsCoimbraClubs = require('./clubs-coimbra.json')
 
 const app = express()
-const coimbraClubIds = airCourtsCoimbraClubs.map((club) => club.id)
 
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -16,79 +12,9 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-app.get('/week-availability/json', async (req, res) => {
-    const mergedAvailabilities = await airCourtsWrapper.getClubsWeekAvailability({
-        clubIds: coimbraClubIds,
-        weekDate: req.query.weekDate,
-        startTime: req.query.startTime,
-        sport: 4 // Padel
-    })
-    res.json(mergedAvailabilities)
-})
-
-app.get('/week-availability', async (req, res) => {
-    const weekDate = req.query.weekDate || new Date()
-    const mergedAvailabilities = await airCourtsWrapper.getClubsWeekAvailability({
-        clubIds: coimbraClubIds,
-        weekDate: weekDate,
-        startTime: req.query.startTime,
-        sport: 4 // Padel
-    })
-    const momentWeekDate = moment(weekDate)
-    res.render('week-availability', {
-        htmlWeekDate: momentWeekDate.format('YYYY-MM-DD'),
-        weekDate: momentWeekDate.format('DD/MM/yyyy'),
-        availabilities: mergedAvailabilities
-    })
-})
-
-app.get('/day-availability', async (req, res) => {
-    const date = moment(req.query.date || new Date())
-    const dateQuery = date.format('YYYY-MM-DD')
-    const params = {
-        TableName: 'slots',
-        IndexName: 'StartDateTimestampGSI', // Specify the GSI name
-        KeyConditionExpression: '#start_date = :start_date',
-        ExpressionAttributeNames: {
-            '#start_date': 'start_date',
-        },
-        ExpressionAttributeValues: {
-            ':start_date': dateQuery, // Replace with your actual start_date
-        },
-    };
-
-    // Perform the query
-    try {
-        const data = await ddb.query(params).promise()
-        res.render('day-availability', {
-            moment: moment,
-            date: dateQuery,
-            htmlWeekDate: dateQuery,
-            slots: data.Items
-        })
-    } catch (error) {
-        console.error('Error querying DynamoDB:', error);
-        res.status(500).send('Error')
-    }
-})
-
-app.post('/sweep', async (req, res) => {
-    try {
-        await lambda.invoke({
-            FunctionName: process.env.CRON_LAMBDA_NAME,
-            InvocationType: 'Event',
-            Payload: JSON.stringify({ date: req.body.date, startTime: req.body.startTime })
-        }).promise()
-        res.status(202).send('Enqueued')
-    } catch (error) {
-        console.error('Error invoking Lambda:', error);
-        res.status(500).send('Error')
-    }
-})
-
-app.get('/', (req, res) => res.render('index'))
-
-app.get('/home', (req, res) => res.redirect('https://aircourts-availability-test-bucket.s3.eu-west-3.amazonaws.com/index.html'))
+const redirectHandler = (req, res) => res.redirect('https://aircourts-availability-test-bucket.s3.eu-west-3.amazonaws.com/index.html')
+app.get('/', redirectHandler)
+app.get('/home', redirectHandler)
 
 app.get('/subscriptions', async (req, res) => {
     res.render('subscription', {
